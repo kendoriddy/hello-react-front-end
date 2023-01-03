02de6adb185f9b5a80e09362fcc67a20c8eea2d0
Kehinde Onifade <onifkay@gmail.com>
2023-01-03 23:47:36 +0100

    update store

--- src/store.js
+++ src/store.js
@@ -1,4 +1,8 @@
 import { configureStore, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { greetingsApi } from "./features/apiSlice";
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    [greetingsApi.reducerPath]: greetingsApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(greetingsApi.middleware),
 });
