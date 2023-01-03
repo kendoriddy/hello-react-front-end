f0e8134e3081af8b67a7c4a5e720877c2d8eb3e1
Kehinde Onifade <onifkay@gmail.com>
2023-01-03 23:22:21 +0100

    add ApiProvider to App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import './App.css';
+import "./App.css";
 import { Data } from "./components/Data";
 import { store } from "./store";
 import { Provider } from "react-redux";
+import { ApiProvider } from "@reduxjs/toolkit/query/react";
+import { greetingsApi } from "./features/apiSlice";
 
 function App() {
   return (
     <Provider store={store}>
+      <ApiProvider api={greetingsApi}>
         <div className="App">
           <Data />
         </div>
+      </ApiProvider>
     </Provider>
   );
 }
--- src/components/Data.js
+++ src/components/Data.js
@@ -1,3 +1,8 @@
+import { useGetGreetingQuery } from "../features/apiSlice"
+
 export const Data = () => {
+
+    const {data} = useGetGreetingQuery()
+    console.log(data)
     return <div>Greeting of the Day:</div>
 }
