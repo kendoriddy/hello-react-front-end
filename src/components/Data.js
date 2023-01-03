3c74dd09c34550475b2a427d567201303f908975
Kehinde Onifade <onifkay@gmail.com>
2023-01-03 23:36:48 +0100

    add styling to Data page

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,7 +1,15 @@
-import { useGetGreetingQuery } from "../features/apiSlice"
+import { useGetGreetingQuery } from "../features/apiSlice";
 
 export const Data = () => {
 
-    const {data} = useGetGreetingQuery()
-    return <div>Greeting of the Day: {data?.name}</div>
-}
+    const handleClick = () => {
+        window.location.reload();
+    }
+  const { data } = useGetGreetingQuery();
+  return (
+    <>
+      <button className="button" onClick={handleClick}>Greet</button>
+      <div>Greeting of the Day: {data?.name}</div>
+    </>
+  );
+};
